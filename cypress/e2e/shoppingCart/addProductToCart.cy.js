fd380e98ec44a9b3315941dfa9232a3d0891d8b1
Amal Butmah <amalbutmah376@gmail.com>
2025-07-02 23:18:36 +0300

    cypess custom commands

--- cypress/e2e/shoppingCart/addProductToCart.cy.js
+++ cypress/e2e/shoppingCart/addProductToCart.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 
 describe('Check Add Product to Cart', () => {
+    before(() => {
+        const email = "CypressUser@gmail.com"
+        const password = "test@123"
+        cy.loginToMagento(email,password)
+    })
     it('Verify that adding product to cart is successful', () => {
         cy.visit("/")
         cy.get("#search").type("shirt")
@@ -10,8 +15,8 @@ describe('Check Add Product to Cart', () => {
         cy.contains("XS").click()
         cy.get('[aria-label="Purple"]').click()
         cy.get("#product-addtocart-button").click()
-        cy.wait(1000)
-        cy.get(".counter.qty").first().should("be.visible").and("contain","1")
+        cy.wait(4000)
+        cy.get(".counter-number").first().should("be.visible")
         cy.get('[role="alert"]').should("be.visible").and("contain","You added Radiant Tee to your shopping cart")
         
     });
