df23641da690c8d575032142d2a7affeaef3a1b9
Amal Butmah <amalbutmah376@gmail.com>
2025-07-17 15:24:01 +0300

    then, wrap, and invoke practice

---
+++ cypress/e2e/cucumber/Tests/printProductsNames.feature
@@ -0,0 +1,5 @@
+Feature: Print product name
+
+    Scenario: Check Products in Hot Seller section
+        Given The user navigated to magento website
+        Then The products should be available in Hot Seller section
---
+++ cypress/e2e/cucumber/Tests/printProductsNames/printProductsNames.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given("The user navigated to magento website",()=>{
+    cy.visit("/");
+})
+
+Then("The products should be available in Hot Seller section",()=>{
+    cy.log( cy.get(".product-item-link"))
+    //const name = $products.text()
+    //cy.log(name)
+    cy.get(".product-item-link").then(($products)=>{
+        for (let i = 0; i < $products.length; i++) {
+            cy.wrap($products[i]).invoke("text").then((productName)=>{
+                cy.log(productName)
+            })
+
+        }
+    })
+})
